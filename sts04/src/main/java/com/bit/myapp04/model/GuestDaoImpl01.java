4437be68c52fd0022cfadd8522155797ed6013a1
hyungjoon90 <hyungjoon112@gmail.com>
2018-06-26 20:50:39 +0900

    sts04 spring ibatis, mybatis 공부 입니다. modal을 사용하여 jsp를 구현하였습니다.

--- sts04/src/main/java/com/bit/myapp04/model/GuestDaoImpl01.java
+++ sts04/src/main/java/com/bit/myapp04/model/GuestDaoImpl01.java
@@ -6,106 +6,120 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.bit.myapp04.model.entity.GuestVo;
 
-public class GuestDaoImpl01 implements GuestDao {
-	JdbcTemplate jdbcTemplate;
-	PlatformTransactionManager transactionManager;
-	
-	
-//	public GuestDaoImpl01(PlatformTransactionManager transactionManager
-//			,TransactionDefinition definition) {
-//		transactionManager.getTransaction(definition);
-//	}
-	
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-	
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
+public class GuestDaoImpl01 extends JdbcDaoSupport implements GuestDao {
 
 	
-	RowMapper<GuestVo> rowMapper=new RowMapper<GuestVo>() {
-		
+	private TransactionTemplate transactionTemplate;
+	
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+	
+	private RowMapper<GuestVo> rowMapper = new RowMapper<GuestVo>() {
 		@Override
 		public GuestVo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			
 			return new GuestVo(
-					rs.getInt("sabun")
-					,rs.getString("name")
-					,rs.getDate("nalja")
-					,rs.getInt("pay")
+						rs.getInt("sabun")	,rs.getString("name")	,rs.getDate("nalja")
+						,rs.getInt("pay")
 					);
-		}
-	};
+		}// mapRow() end
+	};// rowMapper end
+	
 	
 	@Override
 	public List<GuestVo> selectAll() throws SQLException {
-		String sql="SELECT * FROM GUEST ORDER BY SABUN";
-		return jdbcTemplate.query(sql, rowMapper);
+		String sql = "SELECT * FROM GUEST ORDER BY SABUN";
+		return getJdbcTemplate().query(sql, rowMapper );
 	}
 
 	@Override
 	public GuestVo selectOne(int sabun) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM GUEST where sabun = ?";
+		return getJdbcTemplate().query(sql, new Object[] {sabun},rowMapper ).get(0);
 	}
 
-	/// 1000, 1000+111
 	@Override
 	public int insertOne(final GuestVo bean) throws SQLException {
-		final String sql="INSERT INTO GUEST VALUES (?,?,SYSDATE,?)";
+		final String sql ="insert into guest values (?,?,sysdate,?)";
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				getJdbcTemplate().update( new PreparedStatementCreator() {
+
+					@Override
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setInt(1, bean.getSabun());
+						pstmt.setString(2, bean.getName());
+						pstmt.setInt(3, bean.getPay());
+						return pstmt;
+					}});
+				bean.setSabun(bean.getSabun()+333);
+				getJdbcTemplate().update( new PreparedStatementCreator() {
+					
+					@Override
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setInt(1, bean.getSabun());
+						pstmt.setString(2, bean.getName());
+						pstmt.setInt(3, bean.getPay());
+						return pstmt;
+					}});				
+			
+			}});
+
 		
-		TransactionDefinition definition=new DefaultTransactionDefinition();
-		TransactionStatus status=transactionManager.getTransaction(definition);
-		try {
-			jdbcTemplate.update(new PreparedStatementCreator() {
-				
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					pstmt.setInt(1, bean.getSabun());
-					pstmt.setString(2, bean.getName());
-					pstmt.setInt(3, bean.getPay());
-					return pstmt;
-				}
-			});
-			bean.setSabun(bean.getSabun()+333);
-			jdbcTemplate.update(new PreparedStatementCreator() {
-				
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					pstmt.setInt(1, bean.getSabun());
-					pstmt.setString(2, bean.getName());
-					pstmt.setInt(3, bean.getPay());
-					return pstmt;
-				}
-			});
-			transactionManager.commit(status);
-		}catch (Exception e) {
-			transactionManager.rollback(status);
-		}
+		
+		
+//		TransactionDefinition definition = new DefaultTransactionDefinition();
+//		TransactionStatus status = transactionManager.getTransaction(definition );
+//		
+//		try {
+//		jdbcTemplate.update( new PreparedStatementCreator() {
+//
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+//				PreparedStatement pstmt = conn.prepareStatement(sql);
+//				pstmt.setInt(1, bean.getSabun());
+//				pstmt.setString(2, bean.getName());
+//				pstmt.setInt(3, bean.getPay());
+//				return pstmt;
+//			}});
+//		bean.setSabun(bean.getSabun()+333);
+//		jdbcTemplate.update( new PreparedStatementCreator() {
+//			
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+//				PreparedStatement pstmt = conn.prepareStatement(sql);
+//				pstmt.setInt(1, bean.getSabun());
+//				pstmt.setString(2, bean.getName());
+//				pstmt.setInt(3, bean.getPay());
+//				return pstmt;
+//			}});
+//		transactionManager.commit(status);
+//		
+//		}catch (Exception e) {
+//			transactionManager.rollback(status);
+//		}
 		return 0;
 	}
 
 	@Override
 	public int updateOne(final GuestVo bean) throws SQLException {
-		final String sql="UPDATE GUEST SET NAME=?,PAY=? WHERE SABUN=?";
-		return jdbcTemplate.update(new PreparedStatementCreator() {
-			
+		final String sql = "UPDATE GUEST SET NAME=?, PAY=? WHERE SABUN=?";
+		return getJdbcTemplate().update(new PreparedStatementCreator() {
+
 			@Override
 			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
 				PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -113,35 +127,27 @@ public class GuestDaoImpl01 implements GuestDao {
 				pstmt.setInt(2, bean.getPay());
 				pstmt.setInt(3, bean.getSabun());
 				return pstmt;
-			}
-		});
+			}});
 	}
 
 	@Override
 	public int deleteOne(final int sabun) throws SQLException {
-		final String sql="DELETE FROM GUEST WHERE SABUN=?";
-		
-		
-		// »ý¼ºÀÚ¸¦ ÀÌ¿ëÇØ dataSource ÁÖÀÔ
-//		ptm=new DataSourceTransactionManager();
-		
-		TransactionDefinition definition=new DefaultTransactionDefinition();
-		TransactionStatus status=transactionManager.getTransaction(definition);
-		
-		
-		int result=jdbcTemplate.update(new PreparedStatementCreator() {
-			
+		final String sql ="DELETE FROM GUEST WHERE SABUN = ?";
+
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			@Override
-			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setInt(1, sabun);
-				return pstmt;
-			}
-		});
-		transactionManager.commit(status);
-//		transactionManager.rollback(status);
-		
-		return result;
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				getJdbcTemplate().update(new PreparedStatementCreator() {
+					@Override
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						PreparedStatement pstmt = conn.prepareStatement(sql);
+						pstmt.setInt(1, sabun);
+						return pstmt;
+					}});
+				
+			}});
+
+		return 0;
 	}
 
 }
---
+++ sts04/src/main/java/com/bit/myapp04/model/GuestDaoImpl03.java
@@ -0,0 +1,49 @@
+package com.bit.myapp04.model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.bit.myapp04.model.entity.GuestVo;
+
+public class GuestDaoImpl03 implements GuestDao{
+
+	SqlSession sqlSession;
+	
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	@Override
+	public List<GuestVo> selectAll() throws SQLException {
+		return 	sqlSession.selectList("com.bit.myapp04.model.GuestDao.selectAll");
+	}
+
+	@Override
+	public GuestVo selectOne(int sabun) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertOne(GuestVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateOne(GuestVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOne(int sabun) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+}
